0b885339ff92396479ad0bc618eec6cc7dffa220
Walt Woods <woodswalben@gmail.com>
2018-02-16 16:59:31 -0800

    Fixed README.md import when installing via pip

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
 import os
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
-        'rt') as f:
-    long_desc = f.read()
+try:
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
+            'rt') as f:
+        long_desc = f.read()
+except FileNotFoundError:
+    # Doesn't need to be in PyPI package.
+    long_desc = None
 
 setup(
         name='keras-pickle-wrapper',
         packages=['keras_pickle_wrapper'],
         install_requires=['keras', 'h5py'],
-        version='1.0.3',
+        version='1.0.4',
         description='A small library that wraps Keras models to pickle them.',
         long_description=long_desc,
         author='Walt Woods',
